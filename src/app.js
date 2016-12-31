d1f87940e21c329c924fe4437080f6f17e241084
Randy Mikkelsaar <randy.mikkelsaar@ek3.com>
2016-12-31 18:37:54 -0500

    Fixing test code

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ let app = express();
 function configureExpress() {
     app.use(express.static(__dirname + '/client'));
     app.use(express.static('./bower_components'));
+    // Allow CORS
     app.all('/', (req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     });
 }
 
-function configurePaths() {
-    app.get('/test', (req, res) => {
-        res.json({ val: 123, val_two: 'hello world1' });
-    })
-}
+// Set up mock api
+app.get('/test', (req, res) => {
+    res.status(200).send('Test api successfull.');
+})
 
 configureExpress();
 configurePaths()
